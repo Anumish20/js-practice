a489dbda22c15ae3c0c76b08ccb7f71c33328e32
Anumish20 <110711061+Anumish20@users.noreply.github.com>
2026-01-16 10:01:48 +0000

    config done

--- basics/test.js
+++ basics/test.js
@@ -1,1 +1,17 @@
-console.log("Anubhuti")
+// console.log("Anubhuti")
+// const accountId = "12345";
+// let accountName = "Anubhuti's Account";
+// let accountPassword = "securePassword123";
+// accountCity ="Delhi"
+// let accountBalance;
+
+// console.table({ accountId, accountName, accountPassword, accountCity, accountBalance });
+// let score = "23"
+// console.log(typeof score)
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(typeof booleanIsLoggedIn)
+console.log(booleanIsLoggedIn)
