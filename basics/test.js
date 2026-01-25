9d7c3de6841042147373b1fe94bd9f59aa05b748
Anumish20 <110711061+Anumish20@users.noreply.github.com>
2026-01-25 12:50:51 +0000

    config done

--- basics/test.js
+++ basics/test.js
@@ -608,7 +608,7 @@
 //         // console.log(key); index print
 //         console.log(programming[key]);       
 // }
-const coding = ['js','java','python','cpp']
+// const coding = ['js','java','python','cpp']
 // coding.forEach( function (item){
 //         console.log(item);
         
@@ -645,6 +645,52 @@ const coding = ['js','java','python','cpp']
 //         console.log(`Language name is ${item.languageName}, file extension is ${item.fileExtension} and developed by ${item.developedBy}`);
         
 // })
-const myNums = [1,2,3,4,5,6,7,8,9,10]
-const newNums = myNums.filter((num) => num>= 5)
-console.log(newNums);
+// const myNums = [1,2,3,4,5,6,7,8,9,10]
+// // const newNums = myNums.filter((num) => num>= 5)
+// const newNums = myNums.filter ((num) => {
+//         return num >=5;
+// })
+// const  newNums =[]
+// myNums.forEach((num) => {
+//         if (num>4){
+//                 newNums.push(num);
+//         }    
+// });
+// console.log(newNums);
+// higher order functions: map, filter, reduce
+// const books = [
+//     { title: 'Book One', genre: 'Fiction', publish: 1981, edition: 2004 },
+//     { title: 'Book Two', genre: 'Non-Fiction', publish: 1992, edition: 2008 },
+//     { title: 'Book Three', genre: 'History', publish: 1999, edition: 2007 },
+//     { title: 'Book Four', genre: 'Non-Fiction', publish: 1989, edition: 2010 },
+//     { title: 'Book Five', genre: 'Science', publish: 2009, edition: 2014 },
+//     { title: 'Book Six', genre: 'Fiction', publish: 1987, edition: 2010 },
+//     { title: 'Book Seven', genre: 'History', publish: 1986, edition: 1996 },
+//     { title: 'Book Eight', genre: 'Science', publish: 2011, edition: 2016 },
+//     { title: 'Book Nine', genre: 'Non-Fiction', publish: 1981, edition: 1989 },
+//   ];
+
+// let userBooks = books.filter ((bk) => bk.genre === 'Non-Fiction')
+// //  userBooks = books.filter (bk => bk.publish >= 2000)
+//  userBooks = books.filter (bk => {
+//         return bk.publish >= 1995 && bk.genre === 'History'
+
+// })
+// console.log(userBooks);
+
+// const  myNumbers = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
+//  const newNums = myNumbers.map((num) => num+2)
+// const newNums = myNumbers
+//                 .map((num) => num * 10)
+//                 .map((num) => num + 1)
+//                 .filter((num) => num >=30)
+//  console.log(newNums);
+//  reduce method
+const myNums = [1,2,3,4]
+
+const sum = myNums.reduce(function (acc, curr){
+         console.log(`accumulator : ${acc} , current value : ${curr}`);
+        // acc += curr;
+        return acc + curr}, 0 )
+        console.log(sum);
+        
